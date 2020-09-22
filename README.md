2f90b5a2928fd4dfb8db3403c6011d394a2c3806
Geoffrey Ritchey <Geoffrey.Ritchey@gmail.com>
2020-09-22 10:39:34 -0500

    Date fix to not convert to UTC

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Show a summary of chapel attendance for one student
+
+
+Compile and Run with 
+	
+	./mvnw spring-boot:run
+	
+	
+	
+Open a browser and visit
+
+	http://localhost:8080/chapelattendance
+	
+	http://localhost:8080/chapelattendance/greeting?peopleid=000318821
+	
+To create a war file
+
+	mvn package -Pemb
+
--- src/main/java/com/ritchey/simple/web/History.java
+++ src/main/java/com/ritchey/simple/web/History.java
@@ -3,9 +3,6 @@ package com.ritchey.simple.web;
 import java.util.List;
 import java.util.Map;
 
-import lombok.Data;
-
-@Data
 public class History {
 	private List<Map> presentDates;
 	private Double presentSum;
@@ -13,4 +10,46 @@ public class History {
 	private Double tardySum;
 	
 	private Integer goal;
+
+	public List<Map> getPresentDates() {
+		return presentDates;
+	}
+
+	public void setPresentDates(List<Map> presentDates) {
+		this.presentDates = presentDates;
+	}
+
+	public Double getPresentSum() {
+		return presentSum;
+	}
+
+	public void setPresentSum(Double presentSum) {
+		this.presentSum = presentSum;
+	}
+
+	public List<Map> getTardyDates() {
+		return tardyDates;
+	}
+
+	public void setTardyDates(List<Map> tardyDates) {
+		this.tardyDates = tardyDates;
+	}
+
+	public Double getTardySum() {
+		return tardySum;
+	}
+
+	public void setTardySum(Double tardySum) {
+		this.tardySum = tardySum;
+	}
+
+	public Integer getGoal() {
+		return goal;
+	}
+
+	public void setGoal(Integer goal) {
+		this.goal = goal;
+	}
+	
+	
 }
